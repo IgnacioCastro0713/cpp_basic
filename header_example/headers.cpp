a72c437a3b6e05c47193057ac0693965b89dfe02
ignacio <ignaciomenchaca666@gmail.com>
2019-10-09 20:20:11 -0500

    headers in cpp ... cool

---
+++ header_example/headers.cpp
@@ -0,0 +1,11 @@
+#include "headers.h"
+#include <string>
+
+int main ()
+{
+    printl("hello word from header");
+    string total = to_string(sum(10, 15)); // to_string castea un int a string
+    printl("La suma es: " + total);
+    return 0;
+}
+
---
+++ header_example/headers.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void printl(const string &msg)
+{
+    cout << msg << endl;
+}
+
+int sum(int n1, int n2)
+{
+    return n1+n2;
+}
+
+
